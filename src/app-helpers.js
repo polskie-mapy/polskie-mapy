3341f6d331fc8dedc2a35c8d18327c5cc850e934
cvgore <hi@cvgo.re>
2022-10-30 22:02:44 +0000

    beta changes

--- src/app-helpers.js
+++ src/app-helpers.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-undef
 export const APP_COLOR = __APP_COLOR__;
+// eslint-disable-next-line no-undef
 export const APP_VERSION = __APP_VERSION__;
+// eslint-disable-next-line no-undef
 export const APP_BUILT_DATE = __APP_BUILT_DATE__;
